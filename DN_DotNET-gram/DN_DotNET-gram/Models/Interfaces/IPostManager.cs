16fd611bc3fd36e79c4be06065d82fb9587f1f5c
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-14 09:20:51 -0800

    Completed PostManagementService

--- DN_DotNET-gram/DN_DotNET-gram/Models/Interfaces/IPostManager.cs
+++ DN_DotNET-gram/DN_DotNET-gram/Models/Interfaces/IPostManager.cs
@@ -7,20 +7,31 @@ namespace DN_DotNET_gram.Models.Interfaces
 {
     interface IPostManager
     {
-        //Add Post
-        Task CreatePost(Post post);
+        /// <summary>
+        /// Delete post using post.ID as search criteria.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Task</returns>
+        Task DeleteAsync(int id);
 
-        //Edit Post
-        Task EditPost(Post post);
+        /// <summary>
+        /// Finds a post and returns it using post.ID as search criteria.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Post</returns>
+        Task<Post> FindPost(int id);
 
-        //Delete Post
-        Task DeletePost(Post post);
+        /// <summary>
+        /// Returns a list containing all existing Posts.
+        /// </summary>
+        /// <returns>List<Post></Post></returns>
+        Task<List<Post>> GetPosts();
 
-        //Get Post
-        Task<Post> GetPost(int id);
-
-        //Get All Posts
-        Task<IEnumerable<Post>> GetPosts();
-
+        /// <summary>
+        /// Either creates or updates a post.
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns></returns>
+        Task SaveAsync(Post post);
     }
 }
--- DN_DotNET-gram/DN_DotNET-gram/Models/Services/PostManagementService.cs
+++ DN_DotNET-gram/DN_DotNET-gram/Models/Services/PostManagementService.cs
@@ -1,4 +1,6 @@
-﻿using DN_DotNET_gram.Models.Interfaces;
+﻿using DN_DotNET_gram.Data;
+using DN_DotNET_gram.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,31 +8,72 @@ using System.Threading.Tasks;
 
 namespace DN_DotNET_gram.Models.Services
 {
+    /// <summary>
+    /// Handles Post related database services.
+    /// </summary>
     public class PostManagementService : IPostManager
     {
-        public Task CreatePost(Post post)
+        private readonly DotNetgramDBContext _context;
+
+        public PostManagementService(DotNetgramDBContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task DeletePost(Post post)
+        /// <summary>
+        /// Delete post using post.ID as search criteria.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Task</returns>
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            Post post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Remove(post);
+                await _context.SaveChangesAsync();
+            }
         }
 
-        public Task EditPost(Post post)
+        /// <summary>
+        ///  Finds a post and returns it using post.ID as search criteria.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Post</returns>
+        public async Task<Post> FindPost(int id)
         {
-            throw new NotImplementedException();
+            Post post = await _context.Posts.FirstOrDefaultAsync(p => p.ID == id);
+
+            return post;
         }
 
-        public Task<Post> GetPost(int id)
+        /// <summary>
+        /// Returns a list containing all existing Posts.
+        /// </summary>
+        /// <returns>List<Post></returns>
+        public async Task<List<Post>> GetPosts()
         {
-            throw new NotImplementedException();
+            return await _context.Posts.ToListAsync();
         }
 
-        public Task<IEnumerable<Post>> GetPosts()
+        /// <summary>
+        /// Either creates or updates a post.
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns></returns>
+        public async Task SaveAsync(Post post)
         {
-            throw new NotImplementedException();
+            //If existing post isnt found then post is added.
+            if (await _context.Posts.FirstOrDefaultAsync(p => p.ID == post.ID) == null)
+            {
+                _context.Posts.Add(post);
+            }
+            //If existing post is found then post is updated
+            else
+            {
+                _context.Posts.Update(post);
+            }
+            await _context.SaveChangesAsync();
         }
     }
 }
