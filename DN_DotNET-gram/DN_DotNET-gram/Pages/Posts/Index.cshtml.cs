5f1060db9ec125b6f715958aee13c2fbd2f526ed
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-14 10:06:34 -0800

    Completed Post.Index and Post.IndexModel

--- DN_DotNET-gram/DN_DotNET-gram/Pages/Posts/Index.cshtml.cs
+++ DN_DotNET-gram/DN_DotNET-gram/Pages/Posts/Index.cshtml.cs
@@ -1,16 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+using DN_DotNET_gram.Models;
+using DN_DotNET_gram.Models.Interfaces;
 
 namespace DN_DotNET_gram.Pages.Posts
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IPostManager _post;
+
+        public IndexModel(IPostManager post)
         {
+            _post = post;
+        }
+
+        [FromRoute]
+        public int ID { get; set; }
+        public Post post { get; set; }
+
+        public async Task OnGet()
+        {
+            // set all the data for my .cshtml page.
+
+            // Get the specific Restaurant data for the id that was sent. 
+            post = await _post.FindPost(ID);
         }
     }
 }
