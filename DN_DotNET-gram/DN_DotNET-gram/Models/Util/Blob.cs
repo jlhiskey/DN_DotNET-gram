1174be445b9f0445121798dabb636cad5b1400e9
Jason Hiskey <jasonhi@crazyredhead.com>
2019-02-15 06:58:09 -0800

    Completed blob storage last night and forgot to commit...

---
+++ DN_DotNET-gram/DN_DotNET-gram/Models/Util/Blob.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DN_DotNET_gram.Models.Util
+{
+    public class Blob
+    {
+        public CloudStorageAccount CloudStorageAccount { get; set; }
+        public CloudBlobClient CloudBlobClient { get; set; }
+
+        /// <summary>
+        /// Creates a blob connection and blob client using user secrets.
+        /// </summary>
+        /// <param name="configuration"></param>
+        public Blob(IConfiguration configuration)
+        {
+            CloudStorageAccount = CloudStorageAccount.Parse(configuration["ConnectionStrings:BlobConnectionString"]);
+            CloudBlobClient = CloudStorageAccount.CreateCloudBlobClient();
+        }
+
+        /// <summary>
+        /// Creates a new container if it doesnt already exists and sets access permission.
+        /// </summary>
+        /// <param name="containerName"></param>
+        public async Task<CloudBlobContainer> GetContainer(string containerName)
+        {
+            CloudBlobContainer cbc = CloudBlobClient.GetContainerReference(containerName);
+            await cbc.CreateIfNotExistsAsync();
+            await cbc.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+
+            return cbc;
+        }
+
+        /// <summary>
+        /// Retreives a blob from a container.
+        /// </summary>
+        /// <param name="imageName"></param>
+        /// <param name="containerName"></param>
+        /// <returns>CloudBlob</returns>
+        public async Task<CloudBlob> GetBlob(string imageName, string containerName)
+        {
+            CloudBlobContainer container = await GetContainer(containerName);
+
+            CloudBlob blob = container.GetBlobReference(imageName);
+
+            return blob;
+        }
+
+        public void UploadFile(CloudBlobContainer cloudBlobContainer, string fileName, string filePath)
+        {
+            var blobFile = cloudBlobContainer.GetBlockBlobReference(fileName);
+            blobFile.UploadFromFileAsync(filePath);
+        }
+    }
+}
--- DN_DotNET-gram/DN_DotNET-gram/Pages/Posts/Manage.cshtml.cs
+++ DN_DotNET-gram/DN_DotNET-gram/Pages/Posts/Manage.cshtml.cs
@@ -3,6 +3,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
 using DN_DotNET_gram.Models;
 using DN_DotNET_gram.Models.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using DN_DotNET_gram.Models.Util;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage.Blob;
 
 namespace DN_DotNET_gram.Pages.Posts
 {
@@ -10,9 +15,11 @@ namespace DN_DotNET_gram.Pages.Posts
     {
         private readonly IPostManager _post;
 
-        public ManageModel(IPostManager post)
+        public ManageModel(IPostManager post, IConfiguration configuration)
         {
             _post = post;
+
+            BlobImage = new Blob(configuration);
         }
 
         /// <summary>
@@ -26,6 +33,11 @@ namespace DN_DotNET_gram.Pages.Posts
         /// </summary>
         [BindProperty]
         public Post Post { get; set; }
+
+        [BindProperty]
+        public IFormFile Image { get; set; }
+
+        public Blob BlobImage { get; set; }
 
         /// <summary>
         /// Sets the Post
@@ -45,8 +57,28 @@ namespace DN_DotNET_gram.Pages.Posts
             var post = await _post.FindPost(ID.GetValueOrDefault()) ?? new Post();
 
             post.Name = Post.Name;
-            post.URL = Post.URL;
+           
             post.Details = Post.Details;
+
+            if(Image != null)
+            {
+                var filePath = Path.GetTempFileName();
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await Image.CopyToAsync(stream);
+                }
+
+                //Retrieve a container
+                var container = await BlobImage.GetContainer("pictures");
+
+                //Upload image
+                BlobImage.UploadFile(container, Image.FileName, filePath);
+
+                CloudBlob blob = await BlobImage.GetBlob(Image.FileName, container.Name);
+
+                post.URL = blob.Uri.ToString();
+            }
 
             await _post.SaveAsync(post);
 
